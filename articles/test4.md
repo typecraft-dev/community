e2f79e72d2b925606d639f664da04867af18bb43
Robert Beene <robert@ismly.com>
2024-05-30 20:35:37 -0400

    Some more test

--- articles/test4.md
+++ articles/test4.md
@@ -8,6 +8,8 @@ published_at: "2023-05-30"
 #My 4th Awesome Article
 
 ~~some markdown~~
+*some more*
+**and this too**
 
 This is the content of my awesome article #4. So so so good?!?!!!!?!!
 
--- update-articles.mjs
+++ update-articles.mjs
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import GhostAdminAPI from '@tryghost/admin-api';
 import GhostContentAPI from '@tryghost/content-api';
 import { execSync } from 'child_process';
